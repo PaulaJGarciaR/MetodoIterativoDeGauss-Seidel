a52dcf8c6a4bfbd060ad64893cf77572793408d6
PaulaJGarciaR <pjgarciar@ufpso.edu.co>
2024-09-03 22:52:58 -0500

    Cree una función para guardar los valores proporcionados en una matriz y otra función para ordenar la matriz para hallar la diagonal dominante, ya es funcional, faltan las validaciones

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
 import customtkinter
 import numpy
 
-from operations import process
-
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
+#Creación y Configuración de la ventana principal
 app=customtkinter.CTk()
 app.geometry("800x600")
 app.columnconfigure(0,weight=1)
 app.rowconfigure(0,weight=1)
 
-#Titulo
+#Titulo Principal
 title=customtkinter.CTkLabel(master=app,
                              text="Método iterativo de Gauss-Seidel",
                              font=("Impact",35))
 title.grid(row=0,column=0,pady=10)
 
+#Creación de frame principal con desplazamiento vertical
 scrollable_frame = customtkinter.CTkScrollableFrame(master=app, width=780, height=500)
 scrollable_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
-scrollable_frame.grid_columnconfigure(0, weight=1) 
-scrollable_frame.grid_rowconfigure(1, weight=1)  
+scrollable_frame.grid_columnconfigure(0, weight=1)
+scrollable_frame.grid_rowconfigure(1, weight=1)
 
-
-#Crear_frame_principal_y_configuracion_de_tamaño
-frame_principal = customtkinter.CTkFrame(master=scrollable_frame, border_width=0)
-frame_principal.grid(row=1, column=0,sticky="nsew")
-frame_principal.columnconfigure(0,weight=1)
-frame_principal.rowconfigure(0,weight=1)
-
-#Crear_frame_contenedor_del_sistema_de_ecuaciones
-frame_box_system_of_equations=customtkinter.CTkFrame(master=frame_principal, border_width=3,
+#Crear frame contenedor del sistema de ecuaciones
+frame_box_system_of_equations=customtkinter.CTkFrame(master=scrollable_frame, border_width=3,
                                                      border_color="#F2858E")
 frame_box_system_of_equations.grid(row=0, column=0, padx=10, pady=10,sticky="nsew")
 frame_box_system_of_equations.grid_columnconfigure(0,weight=1)
 frame_box_system_of_equations.grid_rowconfigure(0,weight=0)
 
-#Crear_frame_sistema_de_ecuaciones
+#Crear frame sistema de ecuaciones
 frame_system_of_equations=customtkinter.CTkFrame(master=frame_box_system_of_equations)
 frame_system_of_equations.grid(row=1, column=0, padx=10, pady=10)
 frame_system_of_equations.grid_columnconfigure(0,weight=1)
 frame_system_of_equations.grid_rowconfigure(0,weight=0)
 
+#Titulo para el sitema de ecuaciones
 title=customtkinter.CTkLabel(master=frame_box_system_of_equations,
                              text="Sitema de Ecuaciones 3x3",
-                             font=("Lucida Console",20,"bold"))
+                             font=("Lucida Console",22,"bold"))
 title.grid(row=0,column=0,pady=10)
 
 #Ecuacion_Uno
@@ -116,33 +110,70 @@ input_b3=customtkinter.CTkEntry(master=frame_system_of_equations,width=70,
                                 font=("Lucida Console",18))
 input_b3.grid(row=2, column=6,padx=10)
 
-#Funcion_para_enviar_valores_para_hacer_operaciones
-def send_values_of_the_system_of_equations():
-    """Function send values."""
+def save_values ():
+    """Funcion para guardar valores."""
     values_of_the_system_of_equations=numpy.array(
-        [[input_a11.get(),input_a12.get(),input_a13.get(),input_b1.get()],
+        [[float(input_a11.get()),input_a12.get(),input_a13.get(),input_b1.get()],
          [input_a21.get(),input_a22.get(),input_a23.get(),input_b2.get()],
-         [input_a31.get(),input_a32.get(),input_a33.get(),input_b3.get()]]
-    )
-    process.save_value(values_of_the_system_of_equations)
+         [input_a31.get(),input_a32.get(),input_a33.get(),input_b3.get()]])
+    conversion_values_of_the_system_of_equations=values_of_the_system_of_equations.astype(float)
+    return conversion_values_of_the_system_of_equations
 
+def sort_equations():
+    """Funcion para ordenar las ecuaciones."""
+    system_of_equations=save_values()
+    #ordenar ecuacones segun la primera columna
+    column_0=system_of_equations[:,0]
+    indice_fila_max_column_0=numpy.argmax(numpy.abs(column_0))
+    if indice_fila_max_column_0==1:
+        system_of_equations[[0,1]]=system_of_equations[[1,0]]
+    else:
+        if indice_fila_max_column_0==2:
+            system_of_equations[[0,2]]= system_of_equations[[2,0]]
+        else:
+            system_of_equations[[0,0]]= system_of_equations[[0,0]]
+    #ordenar ecuaciones segun la segunda columna
+    column_1=system_of_equations[:,1]
+    indice_fila_max_column_1=numpy.argmax(numpy.abs(column_1))
+    if indice_fila_max_column_1==0:
+        system_of_equations[[0,1]]=system_of_equations[[1,0]]
+    else:
+        if indice_fila_max_column_1==2:
+            system_of_equations[[1,2]]= system_of_equations[[2,1]]
+        else:
+            system_of_equations[[1,1]]= system_of_equations[[1,1]]
 
+    #ordenar ecuaciones segun la tercera columna
+    column_2=system_of_equations[:,2]
+    indice_fila_max_column_2=numpy.argmax(numpy.abs(column_2))
+    if indice_fila_max_column_2==0:
+        system_of_equations[[0,2]]=system_of_equations[[2,0]]
+        print(system_of_equations)
+    else:
+        if indice_fila_max_column_2==1:
+            system_of_equations[[1,2]]= system_of_equations[[2,1]]
+            print(system_of_equations)
+        else:
+            system_of_equations[[2,2]]= system_of_equations[[2,2]]
+            print(system_of_equations)
+            
+    print(system_of_equations)
 #Boton_para_ingresar_los_datos_proporcionados_a_el_sistema_de_ecuaciones
 button_enter=customtkinter.CTkButton(master=frame_box_system_of_equations,text="Enter",
                                      font=("Lucida Console",18,"bold"),
                                      fg_color="#F22E3E",hover_color="#A62940",
-                                     command=send_values_of_the_system_of_equations)
+                                     command=sort_equations)
 button_enter.grid(row=2, column=0,padx=10,pady=10,ipady=5)
 
 #Boton para limpiar los cajas de texto
 button_delete=customtkinter.CTkButton(master=frame_box_system_of_equations,text="Delete",
                                      font=("Lucida Console",18,"bold"),
-                                     fg_color="#F22E3E",hover_color="#A62940",
+                                     fg_color="#F22E3E",hover_color="#A62940"
                                      )
 button_delete.grid(row=3, column=0,padx=10,pady=10,ipady=5)
 
 #frame_solucion_del_sistema_de_ecuaciones
-frame_answers=customtkinter.CTkFrame(master=frame_principal, border_width=3)
+frame_answers=customtkinter.CTkFrame(master=scrollable_frame, border_width=3)
 frame_answers.grid(row=1, column=0, padx=10, pady=10,sticky="nsew")
 frame_answers.grid_columnconfigure(0,weight=1)
 frame_answers.grid_rowconfigure(0,weight=0)
